5322ed45f661d5e813d77387596a430645b30ad4
Yessiho Peter Jerry <gbenahonyessiho@gmail.com>
2026-02-27 15:17:55 +0100

    continued

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -548,7 +548,7 @@ export default function AboutPage() {
               {
                 name:        "Mercy Godwin Udott",
                 credentials: "B.Sc",
-                image:       "/image/team.png",      // ← replace with real photo
+                image:       "/image/team4.jpeg",      // ← replace with real photo
               },
             ].map((founder, index) => (
               <motion.div
