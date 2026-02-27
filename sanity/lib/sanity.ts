9d7797918b71ce4d0e25475410ed404a132f4f1a
Yessiho Peter Jerry <gbenahonyessiho@gmail.com>
2026-02-27 15:49:49 +0100

    Removed Sanity completely

--- sanity/lib/sanity.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@sanity/client"
-
-export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: "2024-01-01", // use today’s date
-  useCdn: true, // `false` if you want fresh content on every request
-})
